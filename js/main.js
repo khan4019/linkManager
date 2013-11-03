cb735e73e7f95bb45c3226af1a4156f4dd7e90e1
Md Khan <khan4019@gmail.com>
2013-11-03 17:55:26 -0600

    added html tags

--- js/main.js
+++ js/main.js
@@ -1,82 +1,89 @@
 (function(){
+
 	window.linkApp = {
 		Models:{},
 		Collections:{},
 		Views:{}
 	};
 
-//Model
-linkApp.Models.Link = Backbone.Model.extend({
-	defaults:{
-		title:'MDN eval',
-		description:'',
-		url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval",
-		created:new Date(),
-		focus:'javascript',
-		notRead:'',
-		levelOfImportance:'',
-		tags:'JavaScript'
+	//Template
+	window.template = function (id) {
+		return _.template($('#'+id).html());
 	}
-});
 
-//Collection
-linkApp.Collections.Links = Backbone.Collection.extend({
-	model: linkApp.Models.Link
-});
+	//Model
+	linkApp.Models.Link = Backbone.Model.extend({
+		defaults:{
+			title:'',
+			description:'',
+			url:'',
+			source:'',
+			created:new Date(),
+			type:'',
+			focus:'javascript',
+			status:'',
+			importance:'',
+			tags:''
+		}
+	});
 
-//view for all link
+	//Collection
+	linkApp.Collections.Links = Backbone.Collection.extend({
+		model: linkApp.Models.Link
+	});
 
-linkApp.Views.Links = Backbone.View.extend({
-	tagName:'ul',
-	initialize:function(){
-		console.log(this.collection);
-	},
-	render:function(){
-		this.collection.each(function(link){
+	//view for all links
+	linkApp.Views.Links = Backbone.View.extend({
+		tagName:'ul',
+		initialize:function(){
+			console.log(this.collection);
+		},
+		render:function(){
+			this.collection.each(this.addOne, this);
+		},
+		addOne:function(link){
+			//create child view and add to the root
 			var linkView = new linkApp.Views.Link({model:link});
 			this.$el.append(linkView.render().el);
-		}, this);
+		}
+	});
 
-		return this;
-	}
-});
-
-//View
-linkApp.Views.Link = Backbone.View.extend({
-	tagName:'li',
-	template:_.template($('#linkTemplate').html()),
-	
-	render:function () {		
-		this.$el.html(this.template(this.model.toJSON()));
-		return this;
-	}
-});
+	//View (single)
+	linkApp.Views.Link = Backbone.View.extend({
+		tagName:'li',
+		template:_.template($('#linkTemplate').html()),		
+		render:function () {		
+			this.$el.html(this.template(this.model.toJSON()));
+			return this;
+		}
+	});
 
 
-linkApp.Collections.Links = new linkApp.Collections.Links([
-	{
-		title:'MDN with', 
-		description:'use of with for presentation', 
-		url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with",
-		tags:'JavaScript'
-	},
-	{
-		title:'MDN eval', 
-		description:'use of with for presentation', 
-		url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with",
-		tags:'JavaScript'
-	},
-	{
-		title:'git hub', 
-		description:'use of with for presentation', 
-		url:"https://github.com/khan4019?tab=repositories",
-		tags:'JavaScript'
-	}
-]);
+	linkApp.Collections.Links = new linkApp.Collections.Links([
+		{
+			title:'MDN with', 
+			description:'use of with for presentation', 
+			url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with",
+			tags:'JavaScript'
+		},
+		{
+			title:'MDN eval', 
+			description:'use of with for presentation', 
+			url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with",
+			tags:'JavaScript'
+		},
+		{
+			title:'git hub', 
+			description:'use of with for presentation', 
+			url:"https://github.com/khan4019?tab=repositories",
+			tags:'JavaScript'
+		}
+	]);
 
-var allLinkView = new linkApp.Views.Links({collection: linkApp.Collections.Links});
+	var allLinkView = new linkApp.Views.Links({collection: linkApp.Collections.Links});
+	allLinkView.render();
 
-//Temporary
-$(document.body).append(allLinkView.render().el);
+	//Temporary
+	$('.links').append(allLinkView.el);
 
 })();
