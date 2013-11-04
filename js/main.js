f70d9fbe6c408cc64e6d50d5e9a7663c991dc8aa
Md Khan <khan4019@gmail.com>
2013-11-03 20:58:08 -0600

    added add link

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@
 			source:'',
 			created:new Date(),
 			contentType:'',
-			focus:'javascript',
+			area:'javascript',
 			completed:0,
 			importance:'',
 			tags:''
@@ -36,7 +36,8 @@
 	linkApp.Views.Links = Backbone.View.extend({
 		tagName:'ul',
 		initialize:function(){
-			console.log(this.collection);
+			//when add a new one render it
+			this.collection.on('add', this.addOne, this);
 		},
 		render:function(){
 			this.collection.each(this.addOne, this);
@@ -45,7 +46,7 @@
 		addOne:function(link){
 			//create child view and add to the root
 			var linkView = new linkApp.Views.Link({model:link});
-			this.$el.append(linkView.render().el);
+			this.$el.prepend(linkView.render().el);
 		}
 	});
 
@@ -85,6 +86,35 @@
 		}
 	});
 
+	//Add new link
+	linkApp.Views.AddLink = Backbone.View.extend({
+		el:'#addLink', 
+		initialize:function(){
+
+		},
+		events:{
+			'submit':'submit'
+		},
+		submit:function(e){
+			e.preventDefault();
+
+			var currentTarget = $(e.currentTarget),
+				newTitle = currentTarget.find('#newLinkTitle').val(),
+				newURL = currentTarget.find('#newLinkURL').val(),
+				newArea = currentTarget.find('#newLinkArea').val(), 
+				newLink = new linkApp.Models.Link({
+					title:newTitle,
+					url: newURL,
+					area:newArea
+				});
+
+			this.collection.add(newLink);
+				
+			//clear the input	
+			currentTarget.find('#newLinkTitle, #newLinkURL, #newLinkArea').val('');	
+			
+		}
+	});
 
 	linkApp.Collections.Links = new linkApp.Collections.Links([
 		{
@@ -107,6 +137,7 @@
 		}
 	]);
 
+	var addLinkView = new linkApp.Views.AddLink({collection:linkApp.Collections.Links});
 	var allLinkView = new linkApp.Views.Links({collection: linkApp.Collections.Links});
 	
 	//push to body
