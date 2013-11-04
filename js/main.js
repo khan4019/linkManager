33dbfce19b7a5e9a7822914c3885a01227836342
Md Khan <khan4019@gmail.com>
2013-11-03 19:35:53 -0600

    added archieve and delete

--- js/main.js
+++ js/main.js
@@ -53,14 +53,31 @@
 	linkApp.Views.Link = Backbone.View.extend({
 		tagName:'li',
 		template:template('linkTemplate'),
+		initialize:function(){
+			this.model.on('change', this.render, this);
+			this.model.on('destroy', this.remove, this);
+		},
 		events:{
-			'click .add10Percent':'add10Percent'
+			'click .add10Percent':'add10Percent',
+			'click .archieve':'archieve',
+			'click .delete':'destroy'
 		},
 		add10Percent:function(){
 			var mod = this.model, 
 				currentLevel = mod.get('completed');
 			if(currentLevel <100) mod.set('completed', currentLevel+10);
 			console.log(mod.get('completed'));
+		},
+		archieve:function(){
+			//archieve it
+			this.remove();
+			console.log('archieved is not implemented. Hence not archieved. but remove. '+this.model.get('title'));
+		},
+		remove:function(){
+			this.$el.remove();
+		},
+		destroy:function(){
+			this.model.destroy();	
 		},
 		render:function () {		
 			this.$el.html(this.template(this.model.toJSON()));
