1582a72ca472d1d51450c990be01ccf2d530095d
Md Khan <khan4019@gmail.com>
2013-11-10 11:33:47 -0600

    added router structure

--- js/routers/linkAppRouters.js
+++ js/routers/linkAppRouters.js
@@ -1,3 +1,55 @@
 (function () {
-	'use strict';	
+	'use strict';
+
+	linkApp.Routers.LinkRoutes = Backbone.Router.extend({
+		routes:{
+			'':'index',
+			'/archieved':'archieved',
+			'/area':'byArea',
+			'/area/:areaKey':'filterByArea',
+			'/importance':'byImportance',
+			'/importance/:importanceKey':'filterByImportance',
+			'/tags':'byTags',
+			'/tags/:tagkey':'filterByTag',
+			'/*other':'default'
+		},
+		index:function(){
+			console.log('hi there');
+		},
+
+		archieved:function(){
+			console.log('all archieved');
+		},
+		
+		byArea:function(){
+			console.log('order by area');
+		},
+		filterByArea:function(areaKey){
+			console.log('Filter by area: '+areaKey);
+		},
+		
+		byImportance:function(){
+			console.log('ordered by importance');
+		},
+		filterByImportance:function(importanceKey){
+			console.log('Filter by importance: '+importanceKey);	
+		},
+		
+		byTags:function(){
+			console.log('Ordered by Tags')
+		},
+		filterByTag:function(tagkey){
+			console.log('Filtered by Tag: '+tagkey);
+		},
+
+		default:function(){
+			console.log('this doesnt exists');
+		}
+	});
+
+	new linkApp.Routers.LinkRoutes;
+	//backbone uses # style routing. Express urses slash style. ()
+	//TODO: Its not done. have to do crappy stuff http://backbonejs.org/#Router-navigate 
+	
+	Backbone.history.start({pushState: true});
 })();
