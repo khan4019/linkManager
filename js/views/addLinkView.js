3889a9ebbb0422106f920933b78980b39a39ad60
Md Khan <khan4019@gmail.com>
2013-11-10 01:50:00 -0600

    added importance level

--- js/views/addLinkView.js
+++ js/views/addLinkView.js
@@ -26,22 +26,27 @@ var linkApp = linkApp || {
 				newTitle = currentTarget.find('#newLinkTitle').val(),
 				newURL = currentTarget.find('#newLinkURL').val(),
 				newArea = currentTarget.find('#newLinkArea').val(),
+				newDescription = currentTarget.find('#newLinkDescription').val(),
 				newSource = currentTarget.find('#newLinkSource').val(),
 				newType = currentTarget.find('#newLinkType').val(),
 				newTags = currentTarget.find('#newLinkTags').val(),
+				newImportance = currentTarget.find('#newLinkImportance').val(),
 				newLink = new linkApp.Models.Link({
 					title:newTitle,
 					url: newURL,
 					area:newArea,
+					description: newDescription,					
 					source:newSource,
 					contentType:newType,
-					tags:newTags
+					tags:newTags,
+					importance:newImportance
 				});
 
 			if(newLink.isValid()){
 				this.collection.add(newLink);
 				newLink.save();
 				$('#addLinkModal').modal('hide');
+				$('#btnNewLinkReset').trigger('click');
 			}
 			else{
 				alert('Failed to submit. Please provide valid Title, URL and area.');
